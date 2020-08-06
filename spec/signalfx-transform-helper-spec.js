6fdc533d26f30b006c665f571c9b3d2fe11a5dd4
Viktor Taranenko <vtaranenko@signalfx.com>
2020-08-06 16:44:53 +0200

    Corrected sanitization

--- spec/signalfx-transform-helper-spec.js
+++ spec/signalfx-transform-helper-spec.js
@@ -4,15 +4,33 @@ const expect = chai.expect;
 
 describe('signalfx-transform-helper', () => {
 
+  it('should sanitize keys', () => {
+    const obj = {'!ke@£y $%^1&': 'value'};
+    const expected = {detail_key1: 'value'};
+    const sanitizedAndPrefixed = signalfxHelper.transformDetails(obj);
+    expect(sanitizedAndPrefixed).to.deep.equal(expected);
+  });
+
   it('should leave flat objects as they are', () => {
-    const obj = {key1: 'value', key2: 5};
-    const expected = {detail_key1: 'value', detail_key2: 5};
+    const obj = {
+      key1: 'value',
+      key2: 5
+    };
+    const expected = {
+      detail_key1: 'value',
+      detail_key2: 5
+    };
     const sanitizedAndPrefixed = signalfxHelper.transformDetails(obj);
     expect(sanitizedAndPrefixed).to.deep.equal(expected);
   });
 
   it('should stringify arrays and objects', () => {
-    const obj = {key1: 'value', key2: 5, key3: {inner1: 42, inner2: 43}, key4: [2, "one"]};
+    const obj = {
+      key1: 'value',
+      key2: 5,
+      key3: {inner1: 42, inner2: 43},
+      key4: [2, "one"]
+    };
     const expected = {
       detail_key1: 'value',
       detail_key2: 5,
