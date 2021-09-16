ac2848e0cb80f843905ab2fcea0a7918ed0534de
Viktor Taranenko <vtaranenko@signalfx.com>
2021-09-16 10:43:48 +0200

    We can't use nulls and booleans as dimension values, so now stringifying them before sending

--- spec/signalfx-transform-helper-spec.js
+++ spec/signalfx-transform-helper-spec.js
@@ -32,22 +32,32 @@ describe('signalfx-transform-helper', () => {
     expect(sanitizedAndPrefixed).to.deep.equal(expected);
   });
 
-  it('should stringify arrays and objects', () => {
+  it('should stringify arrays, objects, nulls and booleans', () => {
     const obj = {
       id: "7bf73129-1428-4cd3-a780-95db273d1602",
       'detail-type': "EC2 Instance State-change Notification",
       detail: {
         key1: 'value',
         key2: 5,
-        key3: {inner1: 42, inner2: 43},
-        key4: [2, "one"]
+        "key2.1": 5.1,
+        key3: {' inner1 ': 42, inner2: null},
+        key4: [2, "one"],
+        key5: [],
+        ' key6 ': ' value space-surrounded ',
+        key7: null,
+        key8: true,
       }
     };
     const expected = {
       detail_key1: 'value',
       detail_key2: 5,
-      detail_key3: '{"inner1":42,"inner2":43}',
-      detail_key4: '[2,"one"]'
+      detail_key2_1: 5.1,
+      detail_key3: '{" inner1 ":42,"inner2":null}',
+      detail_key4: '[2,"one"]',
+      detail_key5: '[]',
+      detail__key6_: ' value space-surrounded ',
+      detail_key7: 'null',
+      detail_key8: 'true',
     };
     const sanitizedAndPrefixed = signalfxHelper.extractDetailsForSfx(obj);
     expect(sanitizedAndPrefixed).to.deep.equal(expected);
